8228b254634fc9df55da658b4206d94203ac14ed
Juuso Mikkonen <juuso.mikkonen@reaktor.com>
2019-02-25 16:35:49 +0200

    Add stylesheet to src and webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const outputDir = path.join(__dirname, 'build/');
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -15,7 +16,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: false
-    })
+    }),
+    new CopyPlugin([
+      { from: 'src/Index.css', to: 'Index.css' }
+    ]),
   ],
   devServer: {
     compress: true,
